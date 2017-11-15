01e25889d301ba76ad546b78498c99e93ad7f4e7
Worranut Duangkaew <bee@500trends.com>
2017-11-15 19:51:04 +0700

    Fix to top page

--- js/myJavaScript.js
+++ js/myJavaScript.js
@@ -1,252 +1,255 @@
-
 //-------------------------- Teacher -------------
-$( document ).ready(function() {
-	$("#actionBtn0").click(function(){
-		var std0 = JSON.parse(myDataPublic0);
-		$("#nameStd0").text(std0.name);
-		$("#detailStd0").html("Subject : "+std0.position +" <br>Detail: " + std0.office);
-		$("#imageStd0").attr("src","images/"+std0.image);
-	
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn0").click(function() {
+        var std0 = JSON.parse(myDataPublic0);
+        $("#nameStd0").text(std0.name);
+        $("#detailStd0").html("Subject : " + std0.position + " <br>Detail: " + std0.office);
+        $("#imageStd0").attr("src", "images/" + std0.image);
+
+    });
+
+    $(".btn-primary").click(function(event) {
+        event.preventDefault();
+    });
+
 });
 //-------------------------std1(x)------------------
-$( document ).ready(function() {
-	$("#actionBtn1").click(function(){
-		var std1 = JSON.parse(myDataPublic1);
-		$("#nameStd1").text(std1.name);
-		$("#detailStd1").html("ตำแหน่ง "+std1.position +" <br>สังกัด " + std1.office);
-		$("#imageStd1").attr("src","images/"+std1.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn1").click(function() {
+        var std1 = JSON.parse(myDataPublic1);
+        $("#nameStd1").text(std1.name);
+        $("#detailStd1").html("ตำแหน่ง " + std1.position + " <br>สังกัด " + std1.office);
+        $("#imageStd1").attr("src", "images/" + std1.image);
+    });
+
 });
 //-----------------std2(xx)-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std3-------------------
-$( document ).ready(function() {
-	$("#actionBtn3").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd3").text(std3.name);
-		$("#detailStd3").html("ตำแหน่ง "+std3.position +" <br>สังกัด " + std3.office);
-		$("#imageStd3").attr("src","images/"+std3.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn3").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd3").text(std3.name);
+        $("#detailStd3").html("ตำแหน่ง " + std3.position + " <br>สังกัด " + std3.office);
+        $("#imageStd3").attr("src", "images/" + std3.image);
+    });
+
 });
 //-----------------std4-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std4 = JSON.parse(myDataPublic4);
-		$("#nameStd4").text(std4.name);
-		$("#detailStd4").html("ตำแหน่ง "+std4.position +" <br>สังกัด " + std4.office);
-		$("#imageStd4").attr("src","images/"+std4.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std4 = JSON.parse(myDataPublic4);
+        $("#nameStd4").text(std4.name);
+        $("#detailStd4").html("ตำแหน่ง " + std4.position + " <br>สังกัด " + std4.office);
+        $("#imageStd4").attr("src", "images/" + std4.image);
+    });
+
 });
 //-----------------std5-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
 //-----------------std2-------------------
-$( document ).ready(function() {
-	$("#actionBtn2").click(function(){
-		var std2 = JSON.parse(myDataPublic2);
-		$("#nameStd2").text(std2.name);
-		$("#detailStd2").html("ตำแหน่ง "+std2.position +" <br>สังกัด " + std2.office);
-		$("#imageStd2").attr("src","images/"+std2.image);
-	});
-    
+$(document).ready(function() {
+    $("#actionBtn2").click(function() {
+        var std2 = JSON.parse(myDataPublic2);
+        $("#nameStd2").text(std2.name);
+        $("#detailStd2").html("ตำแหน่ง " + std2.position + " <br>สังกัด " + std2.office);
+        $("#imageStd2").attr("src", "images/" + std2.image);
+    });
+
 });
